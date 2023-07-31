38271259c91a6c306798b59d99ca7e6ed0dbd997
Pranish Kedia <pranishk12@gmail.com>
2023-07-31 14:51:18 +0100

    feat: added html render template

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'hello flask'
+    #return 'hello flask'
+    return render_template('hello.html', name='Pranish')
 
 if __name__ == '__main__':
     app.run()
