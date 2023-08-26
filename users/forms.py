22c629b51aa062d005a82e9d19800ed9ac7f458a
Pranish Kedia <pranishk12@gmail.com>
2023-08-26 16:06:02 +0100

    feat: Added validation to RegisterForm

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email, Length, ValidationError, EqualTo
+
+def character_check(form,field):
+    excluded_chars = "<&%"
+
+    for char in field.data:
+        if char in excluded_chars:
+            raise ValidationError(f"Character {char} is not allowed.")
 
 class RegisterForm(FlaskForm):
-    username = StringField()
-    password = PasswordField()
+    username = StringField(validators=[DataRequired(), Email(), character_check()])
+    password = PasswordField(validators=[DataRequired(), Length(min=8, max=15), character_check()])
+    confirm_password = PasswordField(validators=[EqualTo('password', message='Both password field must be equal')])
     submit = SubmitField()
